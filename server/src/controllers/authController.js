3daae02a4877cf614ad09dced128a762ce6f34e0
Fernando Antonio <fernando.santos@prodepa.pa.gov.br>
2019-03-27 11:57:50 -0300

    Comentarios organizados no controller de usuarios

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -18,30 +18,34 @@ router.post('/register', async (req, res) => {
     const { nome, email, senha, senha2 } = req.body
     let errors = []
 
-    // Check Required Fields
+    // checando os campos obrigatórios
     if (!nome || !email || !senha) {
       errors.push({ msg: "Por favor preencha todos os campos" })
     }
-    // Check Passwords match
+    // Checando se as senhas coincidem
     if (senha !== senha2) {
       errors.push({ msg: "As senhas não coincidem" })
     }
-    // Check password length
+    // Checando o tamanho minimo das senahs
     if (senha.length < 6) {
       errors.push({ msg: "A senha deve ter ao menos 6 caracteres" })
     }
 
+    // Verificando se há erros no array de erros
     if (errors.length > 0) {
-
       console.log(errors)
+      // Enviando os erros para o cliente
       res.send({ erros: errors })
     } else {
-      // validation passed
+      // Validação OK!
+      // Verificando se há um usuário com o email fornecido para cadastro
       const user = await User.findOne({ email: email })
       if (user) {
+        // Enviando o erro relacionado ao email em uso
         errors.push({ msg: "Este email já está em uso" })
         res.send({ erros: errors })
       } else {
+        // Instanciando um usuário com base no Model do mongoose
         const newUser = new User({
           nome,
           email,
