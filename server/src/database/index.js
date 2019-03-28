6c6375d109b422ffeb21164d921e531c7ab34f30
Fernando Antonio <fernando.acouto@outlook.com>
2019-03-27 22:41:41 -0300

    Criação de usuario (Concluido), Componente de mensagens agora é dinamico

--- server/src/database/index.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-url = `mongodb+srv://fernandoeeu:fernando123@cluster0-ysbfg.mongodb.net/teste-integrador?retryWrites=true`
-mongoose.connect(url, { useNewUrlParser: true })
-mongoose.Promise = global.Promise
-
-module.exports = mongoose
